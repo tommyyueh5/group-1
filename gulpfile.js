aedb1885cc6ec382a1628f5c787663b0426e774f
tommyyue5 <tommyyueh5@gmail.com>
2020-04-13 13:58:41 +0800

    結構改版

--- gulpfile.js
+++ gulpfile.js
@@ -8,51 +8,69 @@ watch = require('gulp-watch');
 fileinclude = require('gulp-file-include');
 
 //搬家
+gulp.task('concatjs', function () {
+    //do sometime
+    gulp.src('./dev/html/**/*.js').pipe(gulp.dest('dest/'))
+});
+
 gulp.task('concatcss', function () {
-    gulp.src('dev/css/*.css').pipe(gulp.dest('dest/css'));
+    //do sometime
+    gulp.src('./dev/css/*.css').pipe(gulp.dest('./dest/css'))
 });
 
-gulp.task('cleanCSS', function () {
-    return gulp.src('dev/css/*.css')   //所有要壓縮的 .css 檔案
-        .pipe(cleanCSS())                   //將 css 最小化
-        .pipe(rename(function (path) {
-            path.basename += ".min";
-        }))
-        .pipe(gulp.dest('dest/min/css')) //壓縮後檔案輸出位置
-});
+//編譯scss
 
-gulp.task('cleanJs', function () {
-    return gulp.src('dev/js/*.js')   //所有要壓縮的 .css 檔案
-        .pipe(cleanCSS())                   //將 css 最小化
-        .pipe(rename(function (path) {
-            path.basename += ".min";
-        }))
-        .pipe(gulp.dest('dest/min/js')) //壓縮後檔案輸出位置
-});
 
-//編譯sass
-gulp.task('sass', ['template'], function () {
-    gulp.src('dev/sass/*.scss')
+gulp.task('sass', function () {
+    gulp.src('./dev/html/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('dev/css'))
-
+        .pipe(gulp.dest('dest/'))
+});
+//壓縮檔案
+gulp.task('minicss', ['sass'], function () {
+    gulp.src('dev/css/*.css')
+        //壓縮
+        .pipe(cleanCSS({
+            compatibility: 'ie8'
+        }))
+        //完成搬家
+        .pipe(gulp.dest('dest/mini/css'));
+    //完成新增檔案命名
+    gulp.src('dest/mini/css/style.css')
+        .pipe(rename('style-min.css'))
+        .pipe(gulp.dest('dest/mini/css'));
+});
+//完成刪除舊檔名
+gulp.task('clean', function () {
+    del(['dest/mini/css/style.css']);
 });
 
-//html template
-var fileinclude = require('gulp-file-include');
-gulp.task('template',['concatcss'], function () {
-    gulp.src(['dev/html/*.html'])
+gulp.task('fileinclude', function () {
+    gulp.src(['dev/html/**/*.html'])
         .pipe(fileinclude({
             prefix: '@@',
             basepath: '@file'
         }))
-        .pipe(gulp.dest('dest/html'));
-});
+        .pipe(gulp.dest('dest/'));
+})
 
-var watch = require('gulp-watch');
 
-gulp.task('watch', function () {
-    gulp.watch('./dev/sass/*.scss', ['sass']);
-    gulp.watch('./dev/css/*.css', ['cleanCSS']);
-    gulp.watch('./dev/js/*.js', ['cleanJs']);
+
+
+//建立預視瀏覽器
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+gulp.task('default', function () {
+    browserSync.init({
+        server: {
+            //根目錄
+            baseDir: "./",
+            index: "dest/index/index.html"
+        }
+    });
+
+    gulp.watch(["sass/*.scss", "sass/**/*.scss","dev/layout/*.scss"], ['sass']).on('change', reload);
+    gulp.watch(["dev/*.html", "dev/**/*.html"], ['fileinclude']).on('change', reload);
+    gulp.watch(["dev/*.js", "dev/**/*.js"], ['concatjs']).on('change', reload);
 });
