bec51210f915b719bf944facbc276cfe3c4657f4
tommyyue5 <tommyyueh5@gmail.com>
2020-04-19 17:30:37 +0800

    fullpage.js-on
    成功

--- gulpfile.js
+++ gulpfile.js
@@ -77,6 +77,7 @@ gulp.task('default', function () {
             proxy: 'http://localhost:3000',
             index: "dest/index/index.html"
             // index: "dest/btn-style/btn-style.html"
+            // index: "dest/index/swiper.html"
         }
     });
 
