1b2be695cd41b421acd5eeaeff46ce90c44f287b
tommyyue5 <tommyyueh5@gmail.com>
2020-04-23 17:35:55 +0800

    Auto stash before merge of "dev" and "origin/dev"

--- dev/html/index/index.js
+++ dev/html/index/index.js
@@ -1,1 +1,29 @@
 document.write("<script type='text/javascript' src='/dev/js/var.js'></script>");
+
+//pagepiling js
+$(document).ready(function () {
+    $('#pagepiling').pagepiling({
+        menu: null,
+        direction: 'vertical',
+        verticalCentered: true,
+        sectionsColor: [],
+        anchors: [],
+        scrollingSpeed: 700,
+        easing: 'swing',
+        loopBottom: false,
+        loopTop: false,
+        css3: true,
+        navigation: false,
+        normalScrollElements: null,
+        normalScrollElementTouchThreshold: 5,
+        touchSensitivity: 5,
+        keyboardScrolling: true,
+        sectionSelector: '.section',
+        animateAnchor: false,
+
+        //events
+        onLeave: function (index, nextIndex, direction) { },
+        afterLoad: function (anchorLink, index) { },
+        afterRender: function () { },
+    });
+});
