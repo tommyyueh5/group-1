4904566024f3ef6b090d01cf5800ff4c8b08d9dc
tommyyue5 <tommyyueh5@gmail.com>
2020-04-24 11:29:17 +0800

    整合
    半成品

--- dev/html/index/index.js
+++ dev/html/index/index.js
@@ -1,29 +1,84 @@
 document.write("<script type='text/javascript' src='/dev/js/var.js'></script>");
 
-//pagepiling js
-$(document).ready(function () {
-    $('#pagepiling').pagepiling({
-        menu: null,
+//swiper---------------------------------------------------------------------
+$(window).ready(function () {
+    var swiper = new Swiper('.swiper-container ', {
         direction: 'vertical',
-        verticalCentered: true,
-        sectionsColor: [],
-        anchors: [],
-        scrollingSpeed: 700,
-        easing: 'swing',
-        loopBottom: false,
-        loopTop: false,
-        css3: true,
-        navigation: false,
-        normalScrollElements: null,
-        normalScrollElementTouchThreshold: 5,
-        touchSensitivity: 5,
-        keyboardScrolling: true,
-        sectionSelector: '.section',
-        animateAnchor: false,
+        speed: 1000,
+        // spaceBetween: 500,
+        autoHeight: true,
+        effect: "slide",
+        watchOverflow: true,
+        loop: true,
+        grabCursor: true,
+        touchAngle: 45,
 
-        //events
-        onLeave: function (index, nextIndex, direction) { },
-        afterLoad: function (anchorLink, index) { },
-        afterRender: function () { },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            // dynamicBullets:true,
+            renderBullet: function (index, className) {
+                // let year = Array.from(document.querySelectorAll('.swiper-slide'))[index];
+                let year = document.querySelectorAll('.data')[index + 1].getAttribute('data-year');
+                return `<span class="${className}"> ${year} </span>`;
+            },
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        cubeEffect: {
+            slideShadows: true,
+            shadow: true,
+        },
+        mousewheel: {
+            forceToAxis: true,
+            invert: true,
+            sensitivity: 1,
+        },
+
     });
-});
+})
+
+
+
+// swiper.on('slideChange',function(){
+//     // swiper.params.effect = "coverflow";
+//     // console.log(123);
+
+//     // swiper.params.effect === "coverflow"
+//     if(swiper.params.effect == "coverflow"){
+//         return swiper.params.effect = 'flip';
+//     }else if(swiper.params.effect == 'flip'){
+//         return swiper.params.effect = 'coverflow';
+//     }
+// })
+
+//pagepiling js
+// $(document).ready(function () {
+//     $('#pagepiling').pagepiling({
+//         menu: null,
+//         direction: 'vertical',
+//         verticalCentered: true,
+//         sectionsColor: [],
+//         anchors: [],
+//         scrollingSpeed: 700,
+//         easing: 'swing',
+//         loopBottom: false,
+//         loopTop: false,
+//         css3: true,
+//         navigation: false,
+//         normalScrollElements: null,
+//         normalScrollElementTouchThreshold: 5,
+//         touchSensitivity: 5,
+//         keyboardScrolling: true,
+//         sectionSelector: '.section',
+//         animateAnchor: false,
+
+//         //events
+//         onLeave: function (index, nextIndex, direction) { },
+//         afterLoad: function (anchorLink, index) { },
+//         afterRender: function () { },
+//     });
+// });
--- gulpfile.js
+++ gulpfile.js
@@ -29,6 +29,10 @@ gulp.task('concatphp', function () {
     //do sometime
     gulp.src('./dev/html/*/PHP/*').pipe(gulp.dest('dest/'))
 });
+gulp.task('concatlatest', function () {
+    //do sometime
+    gulp.src('./dev/html/latest-news/map/**/*').pipe(gulp.dest('dest/latest-news/map/'))
+});
 
 //編譯scss
 
@@ -57,7 +61,7 @@ gulp.task('clean', function () {
     del(['dest/mini/css/style.css']);
 });
 
-gulp.task('fileinclude', function () {
+gulp.task('fileinclude',['concatlatest'], function () {
     gulp.src(['dev/html/**/*.html'])
         .pipe(fileinclude({
             prefix: '@@',
@@ -65,9 +69,6 @@ gulp.task('fileinclude', function () {
         }))
         .pipe(gulp.dest('dest/'));
 })
-
-
-
 
 //建立預視瀏覽器
 var browserSync = require('browser-sync').create();
@@ -80,17 +81,22 @@ gulp.task('default', function () {
             baseDir: "./",
             files: ['**'],
             proxy: 'http://localhost:3000',
-            // index: "dest/index/index.html"
-            index: "dest/forum/forum.html"
+            index: "dest/index/index.html"
             // index: "dest/btn-style/btn-style.html"
-            // index: "dest/index/swiper.html"
             // index: "dest/diseaseHistory/diseaseHistory.html"
+            // index: "dest/game/game.html"
+            // index: "dest/Epidemic-prevention-SOP/Epidemic-prevention-SOP.html"
+            // index: "dest/Epidemic-shop/Epidemic-shop.html"
+            // index: "dest/forum/forum.html"
+            // index: "dest/infectious-Diseases/infectious-Diseases.hmtl"
+            // index: "dest/latest-news/latest-news.html"
+            // index: "dest/welcome.welcome.html"
+            // index: "dest/btn-style/btn-style.html"
 
-            
         }
     });
 
-    gulp.watch(["sass/*.scss", "sass/**/*.scss","dev/layout/**/*.scss","dev/html/**/*.scss"], ['sass']).on('change', reload);
-    gulp.watch(["dev/*.html", "dev/**/*.html"], ['fileinclude','concatHtml','concatimage','concatphp']).on('change', reload);
-    gulp.watch(["dev/*.js", "dev/**/*.js","dev/layout/**/*.js"], ['concatjs']).on('change', reload);
+    gulp.watch(["sass/*.scss", "sass/**/*.scss", "dev/layout/**/*.scss", "dev/html/**/*.scss"], ['sass']).on('change', reload);
+    gulp.watch(["dev/*.html", "dev/**/*.html"], ['fileinclude', 'concatHtml', 'concatimage', 'concatphp']).on('change', reload);
+    gulp.watch(["dev/*.js", "dev/**/*.js", "dev/layout/**/*.js"], ['concatjs']).on('change', reload);
 });
