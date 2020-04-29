c337286dcccb25dbda7a8bea8be9634ff262f72a
jacky77777 <j100401042@gmail.com>
2020-04-29 17:32:05 +0800

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -89,6 +89,7 @@ gulp.task('default', function() {
             baseDir: "./",
             files: ['**'],
             proxy: 'http://localhost:3000',
+<<<<<<< HEAD
             index: "dest/index/index.html"
                 // index: "dest/Back_End/Back_End.html"
                 // index: "dest/Epidemic-prevention-SOP/Epidemic-prevention-SOP.html"
@@ -102,11 +103,34 @@ gulp.task('default', function() {
                 // index: "dest/btn-style/btn-style.html"
                 // index: "dest/Epidemic-shop/Epidemic-shop.html"
                 // index: "dest/forum/forum.html"
+=======
+            // index: "dest/index/index.html"
+            index: "dest/Back_End/Back_End.html"
+            // index: "dest/Epidemic-prevention-SOP/Epidemic-prevention-SOP.html"
+            // index: "dest/btn-style/btn-style.html"
+            // index: "dest/diseaseHistory/diseaseHistory.html"
+            // index: "dest/game/game.html"
+<<<<<<< HEAD
+            // index: "dest/Epidemic-shop/Epidemic-shop.html"
+=======
+>>>>>>> dev
+            // index: "dest/forum/forum.html"
+            // index: "dest/member/member.html"
+            // index: "dest/infectious-Diseases/infectious-Diseases.hmtl"
+            // index: "dest/latest-news/latest-news.html"
+            // index: "dest/welcome.welcome.html"
+            // index: "dest/btn-style/btn-style.html"
+<<<<<<< HEAD
+=======
+            // index: "dest/Epidemic-shop/Epidemic-shop.html"
+            index: "dest/forum/forum.html"
+>>>>>>> e29e4127c4d39d34e8b3ff2d38b87f0bb8166239
                 // index: "dest/member/member.html"
                 // index: "dest/infectious-Diseases/infectious-Diseases.hmtl"
                 // index: "dest/latest-news/latest-news.html"
                 // index: "dest/welcome.welcome.html"
                 // index: "dest/btn-style/btn-style.html"
+>>>>>>> dev
 
         }
     });
