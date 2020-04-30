342225fbe62e41e911f520c6bc389feaad237ed4
jacky77777 <j100401042@gmail.com>
2020-04-30 11:35:43 +0800

    4/30

--- gulpfile.js
+++ gulpfile.js
@@ -95,7 +95,6 @@ gulp.task('default', function() {
             // index: "dest/btn-style/btn-style.html"
             // index: "dest/diseaseHistory/diseaseHistory.html"
             // index: "dest/game/game.html"
-<<<<<<< HEAD
             //index: "dest/Epidemic-shop/Epidemic-shop.html"
             index: "dest/forum/forum.html"
                 // index: "dest/member/member.html"
@@ -104,13 +103,13 @@ gulp.task('default', function() {
                 // index: "dest/welcome.welcome.html"
                 // index: "dest/btn-style/btn-style.html"
 
-=======
+
             // index: "dest/forum/forum.html"
-            index: "dest/member/member.html"
+            // index: "dest/member/member.html"
             // index: "dest/infectious-Diseases/infectious-Diseases.hmtl"
             // index: "dest/welcome/welcome.html"
             // index: "dest/latest-news/latest-news.html"
->>>>>>> 87f14f328253d90d60d7255919395faefaf6e8f0
+
         }
     });
 
