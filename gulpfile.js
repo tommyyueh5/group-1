528068ecf73af41e5c6c92dc4814265c7742f1c9
jacky77777 <j100401042@gmail.com>
2020-04-30 10:42:33 +0800

    4/30

--- gulpfile.js
+++ gulpfile.js
@@ -8,50 +8,50 @@ watch = require('gulp-watch');
 fileinclude = require('gulp-file-include');
 
 //搬家
-gulp.task('concatHtml', function () {
+gulp.task('concatHtml', function() {
     //do sometime
     gulp.src('./dev/html/index/earth/**/*').pipe(gulp.dest('dest/index/earth/'))
 });
-gulp.task('concatjs', function () {
+gulp.task('concatjs', function() {
     //do sometime
     gulp.src('./dev/html/**/*.js').pipe(gulp.dest('dest/'))
     gulp.src('./dev/js/*.js').pipe(gulp.dest('dest/js'))
 });
-gulp.task('concatcss', function () {
+gulp.task('concatcss', function() {
     //do sometime
     gulp.src('./dev/css/*.css').pipe(gulp.dest('dest/css'))
 });
-gulp.task('concatimage', function () {
+gulp.task('concatimage', function() {
     //do sometime
     gulp.src('./dev/image/**/*').pipe(gulp.dest('dest/image'))
     gulp.src('./dev/html/Epidemic-shop/img/*').pipe(gulp.dest('dest/Epidemic-shop/img/'))
 });
-gulp.task('concatphp', function () {
+gulp.task('concatphp', function() {
     //do sometime
     gulp.src('./dev/html/*/PHP/*').pipe(gulp.dest('dest/'))
 });
-gulp.task('concatlatest', function () {
+gulp.task('concatlatest', function() {
     //do sometime
     gulp.src('./dev/html/latest-news/map/**/*').pipe(gulp.dest('dest/latest-news/map/'))
 });
-gulp.task('concatshop_move', function () {
+gulp.task('concatshop_move', function() {
     //do sometime
     gulp.src('./dev/html/Epidemic-shop/img/**/*').pipe(gulp.dest('dest/Epidemic-shop/img'))
 });
-gulp.task('concat_icofont', function () {
+gulp.task('concat_icofont', function() {
     //do sometime
     gulp.src('./dev/html/Epidemic-shop/icofont/**/*').pipe(gulp.dest('dest/Epidemic-shop/icofont'))
 });
 //編譯scss
 
 
-gulp.task('sass', function () {
+gulp.task('sass', function() {
     gulp.src('./dev/html/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('dest/'))
 });
 //壓縮檔案
-gulp.task('minicss', ['sass'], function () {
+gulp.task('minicss', ['sass'], function() {
     gulp.src('dev/css/*.css')
         //壓縮
         .pipe(cleanCSS({
@@ -65,11 +65,11 @@ gulp.task('minicss', ['sass'], function () {
         .pipe(gulp.dest('dest/mini/css'));
 });
 //完成刪除舊檔名
-gulp.task('clean', function () {
+gulp.task('clean', function() {
     del(['dest/mini/css/style.css']);
 });
 
-gulp.task('fileinclude', ['concatlatest'], function () {
+gulp.task('fileinclude', ['concatlatest'], function() {
     gulp.src(['dev/html/**/*.html'])
         .pipe(fileinclude({
             prefix: '@@',
@@ -82,7 +82,7 @@ gulp.task('fileinclude', ['concatlatest'], function () {
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
 
-gulp.task('default', function () {
+gulp.task('default', function() {
     browserSync.init({
         server: {
             //根目錄
@@ -95,26 +95,18 @@ gulp.task('default', function () {
             // index: "dest/btn-style/btn-style.html"
             // index: "dest/diseaseHistory/diseaseHistory.html"
             // index: "dest/game/game.html"
-<<<<<<< HEAD
             //index: "dest/Epidemic-shop/Epidemic-shop.html"
-=======
->>>>>>> 713ab601f474a3d1bef2f4bca761f8cdcac797fd
-            // index: "dest/forum/forum.html"
-            index: "dest/member/member.html"
-            // index: "dest/infectious-Diseases/infectious-Diseases.hmtl"
-<<<<<<< HEAD
-             index: "dest/latest-news/latest-news.html"
-            // index: "dest/welcome.welcome.html"
-            // index: "dest/btn-style/btn-style.html"
+            index: "dest/forum/forum.html"
+                // index: "dest/member/member.html"
+                // index: "dest/infectious-Diseases/infectious-Diseases.hmtl"
+                //  index: "dest/latest-news/latest-news.html"
+                // index: "dest/welcome.welcome.html"
+                // index: "dest/btn-style/btn-style.html"
 
-=======
-            // index: "dest/welcome/welcome.html"
-            // index: "dest/latest-news/latest-news.html"
->>>>>>> 713ab601f474a3d1bef2f4bca761f8cdcac797fd
         }
     });
 
     gulp.watch(["sass/*.scss", "sass/**/*.scss", "dev/layout/**/*.scss", "dev/html/**/*.scss"], ['sass']).on('change', reload);
-    gulp.watch(["dev/*.html", "dev/**/*.html"], ['fileinclude', 'concatHtml', 'concatimage', 'concatphp','concat_icofont']).on('change', reload);
+    gulp.watch(["dev/*.html", "dev/**/*.html"], ['fileinclude', 'concatHtml', 'concatimage', 'concatphp', 'concat_icofont']).on('change', reload);
     gulp.watch(["dev/*.js", "dev/**/*.js", "dev/layout/**/*.js"], ['concatjs']).on('change', reload);
 });
