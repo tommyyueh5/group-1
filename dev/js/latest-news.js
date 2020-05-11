7a1dd39847b7711f3613693eaf2dff52402ad44e
tunfo008 <a677891510@gmail.com>
2020-05-11 17:39:33 +0800

    <> Updata

--- dev/js/latest-news.js
+++ dev/js/latest-news.js
@@ -1,57 +1,88 @@
 // document.write("<script type='text/javascript' src='../dest/js/var.js'></script>");
 
+// --------------------------影片----------------------------
 
-$(function () {
-    $.ajax({
-        type: 'get',
-        url: '/dest/js/map/taiwanmap/index.json',
-        success: function (data) {
-            pushDom(data.data);
+window.addEventListener('load', function(){
+    var current = 0,
+    $preview = $('#preview'),
+    $carouselEl = $('#carousel'),
+    $carouselItems = $carouselEl.children(),
+    carousel = $carouselEl.elastislide({
+        current: current,
+        minItems: 3,
+        onClick: function (el, pos, evt) {
+
+            changeImage(el, pos);
+            evt.preventDefault();
+
         },
+        onReady: function () {
 
+            changeImage($carouselItems.eq(current), current);
+
+        }
     });
 
-    function pushDom(data1) {
-        var vm = new Vue({
-            el: '#app',
-            data: {
-                filter: "",
-                peps: data1
-           
-            },
-        
-            computed: {
-                now_area: function () {
-                    var vobj = this;
-                    
-                    var result = this.peps.filter(function (obj) {
-                      
-                        return obj.number == vobj.filter;
-
-                    });
-                    console.log(result)
-                   
-                    if (result.length == 0) {
-                        return null;
-                        
-                    }
-                    
-                    return result[0];
-                    
-                }
-
-            }
-
-        });
-
-            $("path").mouseenter(function (e) {
-                console.log($(this).attr("data-name"));
-                var tagname = $(this).attr("data-name");
-                vm.filter = tagname;
-        
-        });
+    function changeImage(el, pos) {
+        $preview.attr('src', el.data('preview'));
+        $carouselItems.removeClass('current-img');
+        el.addClass('current-img');
     }
 });
+
+// --------------------------台灣地圖----------------------------
+
+    $(function () {
+        $.ajax({
+            type: 'get',
+            url: '/dest/js/map/taiwanmap/index.json',
+            success: function (data) {
+                pushDom(data.data);
+            },
+
+        });
+
+        function pushDom(data1) {
+            var vm = new Vue({
+                el: '#app',
+                data: {
+                    filter: "",
+                    peps: data1
+                
+                },
+            
+                computed: {
+                    now_area: function () {
+                        var vobj = this;
+
+                        var result = this.peps.filter(function (obj) {
+                        
+                            return obj.number == vobj.filter;
+
+                        });
+                        console.log(result)
+                    
+                        if (result.length == 0) {
+                            return null;
+
+                        }
+
+                        return result[0];
+
+                    }
+
+                }
+
+            });
+
+                $("path").mouseenter(function (e) {
+                    console.log($(this).attr("data-name"));
+                    var tagname = $(this).attr("data-name");
+                    vm.filter = tagname;
+                
+            });
+        }
+    });
 
 // --------------------------新聞頁籤----------------------------
 
@@ -75,11 +106,11 @@ $(function(){
 window.addEventListener('load', ()=>{
     
 
-    let b = $('.myowl-1-1').attr('tar');
-    $(`.owl-all[tar=dress]`).owlCarousel('destroy'); 
-    $(`.owl-all[tar=hand]`).owlCarousel('destroy'); 
-    $(`.owl-all[tar=goggles]`).owlCarousel('destroy'); 
-    $(`.owl-all[tar=mask]`).owlCarousel('destroy'); 
+    // let b = $('.myowl-1-1').attr('tar');
+    // $(`.owl-all[tar=dress]`).owlCarousel('destroy'); 
+    // $(`.owl-all[tar=hand]`).owlCarousel('destroy'); 
+    // $(`.owl-all[tar=goggles]`).owlCarousel('destroy'); 
+    // $(`.owl-all[tar=mask]`).owlCarousel('destroy'); 
 
 
     $('.type-ico img').click(function(){
