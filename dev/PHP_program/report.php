30e4f038462e19bdcbe7fe5d95aee8efa34edc5b
tommyyue5 <tommyyueh5@gmail.com>
2020-05-24 19:36:10 +0800

    forum_done

--- dev/PHP_program/report.php
+++ dev/PHP_program/report.php
@@ -9,12 +9,14 @@ try{
     $memno=$_POST["memno"];
     $artno=$_POST["artno"];
     $reason=$_POST["reason"];
-    $sql = "INSERT INTO `REPORT`(`DIS_NO`,`MEM_NO`,`REP_C_NO`,`VER_SIT`)
-    VALUES (:artno,:memno,:reason,0)";
+    $date=date("Y-m-d");
+    $sql = "INSERT INTO `REPORT`(`DIS_NO`,`MEM_NO`,`REP_C_NO`,`VER_SIT`,`REP_DATE`)
+    VALUES (:artno,:memno,:reason,0,:datet)";
     $report =$pdo->prepare($sql);
     $report ->bindParam(":memno",$memno);
     $report ->bindParam(":reason",$reason);
     $report ->bindParam(":artno",$artno);
+    $report ->bindParam(":datet",$date);
     // $report ->bindParam(":reason",$reason);
     $report -> execute();
     echo "上傳成功";
