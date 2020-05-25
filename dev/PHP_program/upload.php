cd823f90e254c1f5eb32b236021949dcba04db3f
jacky77777 <j100401042@gmail.com>
2020-05-25 17:06:49 +0800

    5/25

--- dev/PHP_program/upload.php
+++ dev/PHP_program/upload.php
@@ -12,12 +12,15 @@ try{
     $sql = "UPDATE `discussion` SET `DIS_IMG_PATH`= :newPath WHERE DIS_IMG_PATH = :oldPath ";
     
     $dest_folder = "./image/forum/";
+    $save_folder = "../../dest/image/forum/";
     $tmp_name = $_FILES["uploads"]["tmp_name"];
     $imgpath =$_FILES["uploads"]["name"];
     $a = explode(".",$_FILES["uploads"]["name"]);
     $name = date('YmdHis').mt_rand(100,999).".".$a[1];
     $uploadfile = $dest_folder.$name;
-    move_uploaded_file($tmp_name,$uploadfile);
+    $savefile = $save_folder.$name;
+    move_uploaded_file($tmp_name,$savefile);
+
     $updatePath = $pdo->prepare($sql);
     $updatePath ->bindParam(":oldPath",$imgpath);
     $updatePath ->bindParam(":newPath",$uploadfile);
