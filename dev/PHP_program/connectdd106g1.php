5876e24f8efb5253dced97fb56e6ac35438fa780
HanboyLee <50396232+HanboyLee@users.noreply.github.com>
2020-05-25 19:05:35 +0800

    del discussion component upDate

--- dev/PHP_program/connectdd106g1.php
+++ dev/PHP_program/connectdd106g1.php
@@ -1,7 +1,7 @@
 <?php
 	$dsn = "mysql:host=localhost;port=3306;dbname=dd106g1;charset=utf8";
-	$user = "dd106g1";
-	$password = "dd106g1";
+	$user = "root";
+	$password = "root";
 	$options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION);
 	$pdo = new PDO( $dsn, $user, $password, $options);  
 ?>
