936ec86f9359fc61b4342e689b006f52022af4f0
TyTTT-T <twitchz0002@gmail.com>
2020-05-26 18:21:07 +0800

    ssss

--- dev/js/shopjs/shop.js
+++ dev/js/shopjs/shop.js
@@ -1,81 +1,81 @@
 
 window.addEventListener('load', ()=>{
     
-    let b = $('.myowl-1-1').attr('tar');
-    $(`.owl-all[tar=clothe]`).owlCarousel('destroy'); 
-    $(`.owl-all[tar=hand]`).owlCarousel('destroy'); 
-    $(`.owl-all[tar=goggle]`).owlCarousel('destroy'); 
-    $(`.owl-all[tar=mask]`).owlCarousel('destroy'); 
+    // let b = $('.myowl-1-1').attr('tar');
+    // // $(`.owl-all[tar=clothe]`).owlCarousel('destroy'); 
+    // // $(`.owl-all[tar=hand]`).owlCarousel('destroy'); 
+    // // $(`.owl-all[tar=goggle]`).owlCarousel('destroy'); 
+    // // $(`.owl-all[tar=mask]`).owlCarousel('destroy'); 
 
 
-    $('.type-ico img').click(function(){
+    // $('.type-ico img').click(function(){
 
-        let tem = $(this).attr("src");
-        let w = $(this).attr("src").charAt(($(this).attr("src").length)-5);
-        let index = $(this).attr("src").length-5;
-        let end = $(this).attr('src').substring(index);
-        let start = $(this).attr('src').substring(0, index);
-        if (w=='b') {
-            $(this).attr('src', start + end.replace('b','w'));
-        } 
+    //     let tem = $(this).attr("src");
+    //     let w = $(this).attr("src").charAt(($(this).attr("src").length)-5);
+    //     let index = $(this).attr("src").length-5;
+    //     let end = $(this).attr('src').substring(index);
+    //     let start = $(this).attr('src').substring(0, index);
+    //     if (w=='b') {
+    //         $(this).attr('src', start + end.replace('b','w'));
+    //     } 
 
-        for ( let i =0 ; i<$('.type-ico img').not(this).length; i++ ) {
-            $('.type-ico img').not(this)[i].setAttribute('src', $('.type-ico img').not(this)[i].getAttribute('src').replace('w.png','b.png'));
-        }
-
-
-
+    //     for ( let i =0 ; i<$('.type-ico img').not(this).length; i++ ) {
+    //         $('.type-ico img').not(this)[i].setAttribute('src', $('.type-ico img').not(this)[i].getAttribute('src').replace('w.png','b.png'));
+    //     }
 
 
 
 
-        $(`.owl-all[tar=${b}]`).owlCarousel('destroy'); 
-        b = $(this).parent().attr('tar');
 
-        $(`.owl-all[tar=${b}]`).owlCarousel({
-            // stagePadding: 50,
-            // margin: 40,
-            nav:true,
+
+
+    //     $(`.owl-all[tar=${b}]`).owlCarousel('destroy'); 
+    //     b = $(this).parent().attr('tar');
+
+    //     $(`.owl-all[tar=${b}]`).owlCarousel({
+    //         // stagePadding: 50,
+    //         // margin: 40,
+    //         nav:true,
             
-            navText: ["<i class='icofont icofont-rounded-left prenext'></i>","<i class='icofont icofont-rounded-right prenext'></i>"],
-            responsive:{
-                0:{
-                    items:1,
-                    margin: 10,
-                },
-                577: {
-                    items:2,
-                    margin: 0,
-                },
-                768: {
-                    items:1,
-                    margin: 50,
-                },
-                980: {
-                    items:1,
-                    margin: 0,
-                },
-                992:{
-                    items:2,
-                    margin: 50,
-                },
+    //         navText: ["<i class='icofont icofont-rounded-left prenext'></i>","<i class='icofont icofont-rounded-right prenext'></i>"],
+    //         responsive:{
+    //             0:{
+    //                 items:1,
+    //                 margin: 10,
+    //             },
+    //             577: {
+    //                 items:2,
+    //                 margin: 0,
+    //             },
+    //             768: {
+    //                 items:1,
+    //                 margin: 50,
+    //             },
+    //             980: {
+    //                 items:1,
+    //                 margin: 0,
+    //             },
+    //             992:{
+    //                 items:2,
+    //                 margin: 50,
+    //             },
                 
-                1200:{
-                    items:2,
-                    margin: 0,
-                },
-                1400:{
-                    items:2,
-                    margin: 0,
-                }
-            }
-        });
+    //             1200:{
+    //                 items:2,
+    //                 margin: 0,
+    //             },
+    //             1400:{
+    //                 items:2,
+    //                 margin: 0,
+    //             }
+    //         }
+    //     });
 
 
 
-        //rerwd
+    //     //rerwd
         
-    });
+    // });
 
 
 
@@ -643,11 +643,12 @@ function getproduct() {
                     }
                 }
                 
-                
-                $(`.owl-all[tar=clothe]`).owlCarousel('destroy'); 
-                $(`.owl-all[tar=hand]`).owlCarousel('destroy'); 
-                $(`.owl-all[tar=goggle]`).owlCarousel('destroy'); 
-                $(`.owl-all[tar=mask]`).owlCarousel('destroy'); 
+                // =====================
+                // $(`.owl-all[tar=clothe]`).owlCarousel('destroy'); 
+                // $(`.owl-all[tar=hand]`).owlCarousel('destroy'); 
+                // $(`.owl-all[tar=goggle]`).owlCarousel('destroy'); 
+                // $(`.owl-all[tar=mask]`).owlCarousel('destroy'); 
+                // console.log(5555);
                 $('.myowl-1-1').owlCarousel({
                     
                     nav:true,
@@ -685,7 +686,86 @@ function getproduct() {
                         }
                     }
                 });
+                //========================
 
+
+                
+                let b = $('.myowl-1-1').attr('tar');
+                $(`.owl-all[tar=clothe]`).owlCarousel('destroy'); 
+                $(`.owl-all[tar=hand]`).owlCarousel('destroy'); 
+                $(`.owl-all[tar=goggle]`).owlCarousel('destroy'); 
+                $(`.owl-all[tar=mask]`).owlCarousel('destroy'); 
+            
+            
+                $('.type-ico img').click(function(){
+            
+                    let tem = $(this).attr("src");
+                    let w = $(this).attr("src").charAt(($(this).attr("src").length)-5);
+                    let index = $(this).attr("src").length-5;
+                    let end = $(this).attr('src').substring(index);
+                    let start = $(this).attr('src').substring(0, index);
+                    if (w=='b') {
+                        $(this).attr('src', start + end.replace('b','w'));
+                    } 
+            
+                    for ( let i =0 ; i<$('.type-ico img').not(this).length; i++ ) {
+                        $('.type-ico img').not(this)[i].setAttribute('src', $('.type-ico img').not(this)[i].getAttribute('src').replace('w.png','b.png'));
+                    }
+            
+            
+            
+            
+            
+            
+            
+                    $(`.owl-all[tar=${b}]`).owlCarousel('destroy'); 
+                    b = $(this).parent().attr('tar');
+            
+                    $(`.owl-all[tar=${b}]`).owlCarousel({
+                        // stagePadding: 50,
+                        // margin: 40,
+                        nav:true,
+                        
+                        navText: ["<i class='icofont icofont-rounded-left prenext'></i>","<i class='icofont icofont-rounded-right prenext'></i>"],
+                        responsive:{
+                            0:{
+                                items:1,
+                                margin: 10,
+                            },
+                            577: {
+                                items:2,
+                                margin: 0,
+                            },
+                            768: {
+                                items:1,
+                                margin: 50,
+                            },
+                            980: {
+                                items:1,
+                                margin: 0,
+                            },
+                            992:{
+                                items:2,
+                                margin: 50,
+                            },
+                            
+                            1200:{
+                                items:2,
+                                margin: 0,
+                            },
+                            1400:{
+                                items:2,
+                                margin: 0,
+                            }
+                        }
+                    });
+            
+            
+            
+                    //rerwd
+                    
+                });
+                //========
             } else {
                 alert(conn.status);
             }
