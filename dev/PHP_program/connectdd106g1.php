22b6db9efb8fda3e5ef991af923435b117aa7d9b
jacky77777 <j100401042@gmail.com>
2020-05-27 17:54:02 +0800

    5/27

--- dev/PHP_program/connectdd106g1.php
+++ dev/PHP_program/connectdd106g1.php
@@ -1,9 +1,5 @@
 <?php
-<<<<<<< HEAD
 	$dsn = "mysql:host=localhost;port=8888;dbname=dd106g1;charset=utf8";
-=======
-	$dsn = "mysql:host=localhost;port=8889;dbname=dd106g1;charset=utf8";
->>>>>>> b79327e055dc1e6c118363ee78b809a94817a6b6
 	$user = "root";
 	$password = "root";
 	$options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION);
